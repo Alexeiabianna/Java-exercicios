59a7e124c18a200a8f8e93b1cce6e4b7c071672f
Alexei Abianna <alexeiabianna@gmail.com>
2020-05-07 20:46:07 -0300

    For into for

---
+++ LacoAninhadoEx03.java
@@ -0,0 +1,13 @@
+public class LacoAninhadoEx03 {
+    public static void main(String[] args){
+
+        // Print List number sequence
+
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                System.out.println(i + j);
+            }
+            System.out.println();
+        }
+    }
+}
