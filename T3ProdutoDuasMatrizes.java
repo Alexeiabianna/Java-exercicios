be754661f2b77c0a0ab78829b0063aec71fc8a4a
Alexei Abianna <alexeiabianna@gmail.com>
2020-06-23 21:34:44 -0300

    Trabalho T3 revisado

--- T3ProdutoDuasMatrizes.java
+++ T3ProdutoDuasMatrizes.java
@@ -1,7 +1,10 @@
-// Nomes: Alexei Abianna; Pedro Nunes
-// Trabalho 3
-// Disciplina Fundamentos de Programação
-// Primeira parte.
+/* Esse programa cria e multiplica duas matrizes
+ *
+ * Nomes: Alexei Abianna; Pedro Nunes
+ * Trabalho 3
+ * Disciplina Fundamentos de Programação
+ * Primeira parte.
+ */
 
 import java.util.Arrays;
 
@@ -26,23 +29,30 @@ public class T3ProdutoDuasMatrizes {
      */
 
     public static double[][] produtoMatrizes(double[][] matriz1, double[][] matriz2) {
+        // Compara se a matriz pode ser multiplicada
         if(matriz1[0].length != matriz2.length){
             System.out.println("Dimensões inconsistentes!");
             return null;
         }
-        double[][] r = new double[ matriz1.length ][ matriz2[0].length] ;
 
+        double[][] matrizFinal = new double[ matriz1.length ][ matriz2[0].length] ;
+
+        // Passa por todas as colunas
         for(int i = 0; i < matriz1.length; i++){
+            //Passa por todas as linhas
             for(int j = 0; j <matriz2[0].length; j++){
-                double s = 0;
+                double valor = 0;
+
+                // Esse laço passa multiplica e soma os elementos para multiplicação da matriz
                 for(int k = 0; k < matriz1[0].length; k++){
+                    // Calcula cada elemento da matriz
                     double produto = matriz1[i][k] * matriz2[k][j];
-                    s += produto;
+                    valor += produto;
                 }
-                r[i][j] = s;
+                matrizFinal[i][j] = valor;
             }
         }
 
-        return r;
+        return matrizFinal;
     }
 }
