cbf66e1c5dc55c77d091588953368d84adc36e43
Alexei Abianna <alexeiabianna@gmail.com>
2020-06-24 16:07:17 -0300

    Trabalho T3 BuscaBinária concluído

--- T3BinarySearch.java
+++ T3BinarySearch.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 /**
  * Essa programa verifica se um numero pertence a um array por meio de busca binaria.
  *
@@ -14,19 +12,19 @@ public class T3BinarySearch{
             v[i] = i * i;
         }
 
-        System.out.println(Arrays.toString(v));
+        System.out.println("O vetor tem os quadrados de 0 ate 999 de forma crescente.");
         System.out.println();
         System.out.print("Índices dos valores encontrados: ");
         System.out.println();
-        System.out.print("  " + buscaBinaria(625, v));
-        System.out.print("  " + buscaBinaria(25, v));
-        System.out.print("  " + buscaBinaria(64, v));
-        System.out.print("  " + buscaBinaria(13, v));
-        System.out.print("  " + buscaBinaria(-10, v));
-        System.out.print("  " + buscaBinaria(345744, v));
-        System.out.print("  " + buscaBinaria(-23441, v));
-        System.out.print("  " + buscaBinaria(1001, v));
-        System.out.print("  " + buscaBinaria(0, v));
+        System.out.println("Alvo 625 posição: " + buscaBinaria(625, v));
+        System.out.println("Alvo 25 posição: " + buscaBinaria(25, v));
+        System.out.println("Alvo 65 posição: " + buscaBinaria(64, v));
+        System.out.println("Alvo 13 posição: " + buscaBinaria(13, v));
+        System.out.println("Alvo -10 posição: " + buscaBinaria(-10, v));
+        System.out.println("Alvo 345744 posição: " + buscaBinaria(345744, v));
+        System.out.println("Alvo -23441 posição: " + buscaBinaria(-23441, v));
+        System.out.println("Alvo 1001 posição: " + buscaBinaria(1001, v));
+        System.out.println("Alvo 0 posição: " + buscaBinaria(0, v));
         System.out.println();
     }
 
